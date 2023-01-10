50e551d6ec085de53308628fd48cd1a0cf581d9d
A7Codes <A7codes@gmail.com>
2023-01-10 17:48:04 +0300

    Testing

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+app.listen(process.env.PORT, () => {console.log("Server is running on port " + process.env.PORT)});
+
+app.get("/", function(req, res){
+    res.send("Server Online")
+});
