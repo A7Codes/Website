e261237d66c847bccb46ae411e9f57266d240a8b
A7Codes <A7codes@gmail.com>
2023-01-10 18:31:45 +0300

    Second Edition

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require('dotenv').config();
 app.listen(process.env.PORT, () => {console.log("Server is running on port " + process.env.PORT)});
 
 app.get("/", function(req, res){
-    res.send("Server Online")
+    res.send("Ahmed's Server Online")
 });
